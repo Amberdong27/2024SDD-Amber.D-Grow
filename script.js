77c974a5701bbaf3ec19a999ef8fd805d528c3ec
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-01-12 19:52:57 +1100

    Added two more html and linked
    - added planner.html for planner page and achievement.html for achievement page
    - linked button to respective pages

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function timer() {
     window.location.href="./timer.html";
     }
+    
+function planner() {
+    window.location.href="./planner.html";
+}
+function achievement() {
+    window.location.href="./achievement.html";
+    }
