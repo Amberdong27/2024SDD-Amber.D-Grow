77b0cca39f7fe8b7560c1b613cea3e69bbe36a61
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-01-24 00:25:06 +1100

    Adding Pot Image
    -Added Pot Image
    Source: https://www.pngkit.com/view/u2q8e6y3r5y3e6a9_soil-in-a-pot-png-transparent-soil-in/
    - Testing Changing Images using Buttons

---
+++ timer.js
@@ -0,0 +1,6 @@
+const Timer1 = document.getElementById("Timer1");
+const plantGraphic = document.getElementById("plantGraphic");
+
+Timer1.addEventListener("click", function() {
+    plantGraphic.src = "images\watering.gif";
+});
