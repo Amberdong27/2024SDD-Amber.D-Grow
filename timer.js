2d42735a71131175f814a252f2dcee237d2d3afb
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-01-29 15:13:50 +1100

    Long Method to disabling and hiding buttons
    Still trying to figure out a faster way in processing this

--- timer.js
+++ timer.js
@@ -1,30 +1,33 @@
-var B1 = document.getElementById('B1');
-var B2 = document.getElementById('B2');
-var B3 = document.getElementById('B3');
-var pause = document.getElementById('Pause');
-var end = document.getElementById('End');
-
 function Timer1 () {
-  
-
-  /* document.getElementById('plantGraphic').src = "/images/altpot.gif";
+  document.getElementById('plantGraphic').src = "/images/altpot.gif";
   document.getElementById('B1').disabled = true;
+  document.getElementById('B1').style.visibility = "hidden";
   document.getElementById('B2').disabled = true;
+  document.getElementById('B2').style.visibility = "hidden";
   document.getElementById('B3').disabled = true;
-  document.getElementById('Pause').disabled = false;
-  document.getElementById('Pause').style.visibility = "visible"; */
+  document.getElementById('B3').style.visibility = "hidden";
+  document.getElementById('pause').disabled = false;
+  document.getElementById('pause').style.visibility = "visible";
+  document.getElementById('end').disabled = false;
+  document.getElementById('end').style.visibility = "visible";
+  document.getElementById('restart').disabled = false;
+  document.getElementById('restart').style.visibility = "visible";
+
+
 }
 
 function Timer2 () {
-  document.getElementById('plantGraphic').src = "/images/altpot.gif";
+  /*document.getElementById('plantGraphic').src = "/images/altpot.gif";
   document.getElementById('B1').disabled = true;
   document.getElementById('B2').disabled = true;
-  document.getElementById('B3').disabled = true;
+  document.getElementById('B3').disabled = true; */
 }
 
 function Timer3 () {
-  document.getElementById('plantGraphic').src = "/images/altpot.gif";
+ /* document.getElementById('plantGraphic').src = "/images/altpot.gif";
   document.getElementById('B1').disabled = true;
   document.getElementById('B2').disabled = true;
-  document.getElementById('B3').disabled = true;
+  document.getElementById('B3').disabled = true; */
 } 
+
+
