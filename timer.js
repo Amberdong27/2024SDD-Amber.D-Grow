1fbda8a1674aa63a07aa7d933a5627c051d19433
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-01-29 12:55:56 +1100

    Disabling Buttons
    Disabling other options buttons after one is pressed

--- timer.js
+++ timer.js
@@ -1,3 +1,20 @@
  function Timer1 () {
   document.getElementById('plantGraphic').src = "/images/altpot.gif";
+  document.getElementById('B1').disabled = true;
+  document.getElementById('B2').disabled = true;
+  document.getElementById('B3').disabled = true;
+}
+
+function Timer2 () {
+  document.getElementById('plantGraphic').src = "/images/altpot.gif";
+  document.getElementById('B1').disabled = true;
+  document.getElementById('B2').disabled = true;
+  document.getElementById('B3').disabled = true;
+}
+
+function Timer3 () {
+  document.getElementById('plantGraphic').src = "/images/altpot.gif";
+  document.getElementById('B1').disabled = true;
+  document.getElementById('B2').disabled = true;
+  document.getElementById('B3').disabled = true;
 }
