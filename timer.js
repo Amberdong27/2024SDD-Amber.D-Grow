49d49f0952cde4fdaec0415f2ea083f80945cd93
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-01-29 13:24:34 +1100

    Added Buttons to End and Pause & Testing
    - Buttons to End and Pause
    - Testing other ways to make buttons visible and disabling and enabling buttons

--- timer.js
+++ timer.js
@@ -1,8 +1,18 @@
- function Timer1 () {
-  document.getElementById('plantGraphic').src = "/images/altpot.gif";
+var B1 = document.getElementById('B1');
+var B2 = document.getElementById('B2');
+var B3 = document.getElementById('B3');
+var pause = document.getElementById('Pause');
+var end = document.getElementById('End');
+
+function Timer1 () {
+  
+
+  /* document.getElementById('plantGraphic').src = "/images/altpot.gif";
   document.getElementById('B1').disabled = true;
   document.getElementById('B2').disabled = true;
   document.getElementById('B3').disabled = true;
+  document.getElementById('Pause').disabled = false;
+  document.getElementById('Pause').style.visibility = "visible"; */
 }
 
 function Timer2 () {
@@ -17,4 +27,4 @@ function Timer3 () {
   document.getElementById('B1').disabled = true;
   document.getElementById('B2').disabled = true;
   document.getElementById('B3').disabled = true;
-}
+} 
