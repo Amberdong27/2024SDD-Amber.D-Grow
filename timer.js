f78e7aff09004e4c8ac1f3dc4f33c71e7f181b43
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-02-08 14:54:06 +1100

    Update timer.js
    - Working Version of a Countdown implemented
    
    Reference:
    https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript

--- timer.js
+++ timer.js
@@ -32,11 +32,20 @@ function Timer(clicked) {
   comment.innerHTML = 'Growing...';
 
   time = clicked;
-  startTime = new Date();
-  document.getElementById('countdown').innerHTML = startTime.toLocaleTimeString();
-  
+
  if (time == 'b1') { 
+ R();
   
+  /* startTime = new Date();
+  endTime= newDate();
+
+  endTime.setHours (0);
+  endTime.setMinutes (startTime.getMinutes() + 1);
+  endTime.setSeconds(0);
+  success();
+
+
+  setInterval(T, 1000);
    /* var timeleft = 90;
       var minute = Math.floor(timeleft/60);
       var second = timeleft - minute*60;
@@ -51,7 +60,12 @@ function Timer(clicked) {
       } else {
         timeleft--;
       } 
-    } */
+    } 
+
+    function T() {
+  document.getElementById('countdown').innerHTML = endTime.toLocaleTimeString();
+}
+    */
   } 
   else if (time == 'b2') {
     alert("1 hour");
@@ -60,6 +74,32 @@ function Timer(clicked) {
     alert("2 hour");
   } 
 }
+
+function R() {
+  thirtyMin = 60 * 1,
+  startTimer(thirtyMin);
+};
+
+function startTimer(duration) {
+  var timer = duration, minutes, seconds;
+  setInterval(function R() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    console.log(minutes + ":" + seconds);
+
+    if (--timer < 0){
+      timer = duration;
+    }
+  }, 1000);
+}
+
+
+
+
 function success() {
   plantGraphic.src = "images/Plant.gif";
   comment.innerHTML = "The plant is fully grown!";
@@ -94,7 +134,7 @@ function End() {
   again.style.visibility = "visible";
 }
 function Restart() {
-  
+
 }
 
 function Again() {
