f2874126b6953f81a5ec93e5cee8575d895b738f
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-02-19 11:27:49 +1100

    Adding data to the database

---
+++ myPWA/.database/myQuery.sql
@@ -0,0 +1,6 @@
+--sample commands
+--CREATE TABLE studytips (tipID INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, details TEXT NOT NULL);
+--INSERT INTO studytips (tipID, name, details) VALUES (1,"Taking Breaks","It is important to take short breaks during a study session as it allows the brain to retain information better. While taking breaks focus on doing something active and different."); 
+--INSERT INTO studytips (tipID, name, details) VALUES (2, "Concentration", "To improve concentration use should: minimise distractions, write a to do list and schedule specific study times to help");
+--INSERT INTO studytips (tipID, name, details) VALUES (3, "Organising Workload", "To help with studying you should organise your workload accordingly to what must be done first to what is least important. Writing a list of when things are need to be done and when it's due is also a good way in organising workload");
+SELECT * FROM studytips
