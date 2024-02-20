b6aee27c7ac8773c7008ffeac85ad5a32ed87dbc
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-02-21 00:23:25 +1100

    Finishing Study Tips Page
    - Successfully pulls items from database and displays them
    - Selector works where changing the item will show its corresponding tip
    - CSS styling for the tip boxes

--- myPWA/index.js
+++ myPWA/index.js
@@ -1,3 +1,28 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('.database/datasource.db');
+
+let myString = '[\n';
+db.all("SELECT * FROM studytips", function(err, rows) {
+    let myCounter = 0;
+    rows.forEach(function (row){
+        myString = myString + '{\n "tipID": '+ row.tipID + ',\n "name":"'+ row.name + '",\n "details":"'+ row.details; +",\n";
+        myCounter++;
+        if (myCounter == rows.length){
+            myString = myString + '"\n}\n';
+        } else  {
+            myString = myString + '"\n},\n';
+        }
+    });
+    var fs = require('fs');
+    fs.writeFile ("public/frontEndData.json", myString + "]", function (err){
+    if (err) {
+        console.log(err);
+    }
+}
+);
+});
+
+
 const express = require("express");
 const path = require("path");
 const app = express();
--- myPWA/public/js/studytips.js
+++ myPWA/public/js/studytips.js
@@ -1,3 +1,4 @@
+
 let result = ""; 
 
 fetch('./frontEndData.json') 
@@ -13,15 +14,37 @@ fetch('./frontEndData.json')
   }); 
 
   function appendData(data) { 
-    data.forEach(({tipID, name} = rows) => { 
+    data.forEach(({tipID, name, details} = rows) => { 
       result += ` 
-      <div class=""> 
-      <select class="form-select" aria-label="Default select example">
-      <option selected> Select a study tip</option>
-      <option value = ${tipID} class="card-name">${name}</option>
-      </select>
-  
+       <div id=${tipID} class="card"> 
+            <h2 class="card-name">${name}</h2> 
+            <p class="card-about">${details}</p>  
+        </div> 
        `; 
     }); 
-    document.querySelector(".container").innerHTML = result; 
+    document.getElementById("advice").innerHTML = result; 
   } 
+
+  function selection() {
+    var value = document.getElementById("selector").value;
+    var option1 = document.getElementById("1");
+    var option2 = document.getElementById("2");
+    var option3 = document.getElementById("3");
+    if (value == "1") {
+      option1.style.visibility = "visible";
+      option2.style.visibility = "hidden";
+      option3.style.visibility = "hidden";
+    } else if (value == "2") {
+      option1.style.visibility = "hidden";
+      option2.style.visibility = "visible";
+      option3.style.visibility = "hidden";
+    } else if (value == "3") {
+      option1.style.visibility = "hidden";
+      option2.style.visibility = "hidden";
+      option3.style.visibility = "visible";
+    } else {
+      option1.style.visibility = "hidden";
+      option2.style.visibility = "hidden";
+      option3.style.visibility = "hidden";
+    }
+  }
