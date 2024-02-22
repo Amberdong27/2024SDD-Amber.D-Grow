58eeaa903b94bb852f3a74ce161a6aae631e4f00
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-02-22 11:59:32 +1100

    Working on Notification Support for Android

--- myPWA/public/serviceWorker.js
+++ myPWA/public/serviceWorker.js
@@ -21,3 +21,50 @@ if ("serviceWorker" in navigator) {
   } else {
     console.error("Service workers are not supported.");
   }
+
+  const CATALOGUE_ASSETS = "catalogue-assets"; 
+
+self.addEventListener("install", (installEvt) => { 
+  installEvt.waitUntil( 
+    caches 
+      .open(CATALOGUE_ASSETS) 
+      .then((cache) => { 
+        console.log(cache) 
+        cache.addAll(assets); 
+      }) 
+      .then(self.skipWaiting()) 
+      .catch((e) => { 
+        console.log(e); 
+      }) 
+  ); 
+}); 
+
+
+self.addEventListener("activate", function (evt) { 
+  evt.waitUntil( 
+    caches 
+      .keys() 
+      .then((keyList) => { 
+        return Promise.all( 
+          keyList.map((key) => { 
+            if (key === CATALOGUE_ASSETS) { 
+              console.log("Removed old cache from", key); 
+              return caches.delete(key); 
+            } 
+          }) 
+        ); 
+      }) 
+      .then(() => self.clients.claim()) 
+  ); 
+}); 
+
+
+self.addEventListener("fetch", function (evt) { 
+  evt.respondWith( 
+    fetch(evt.request).catch(() => { 
+      return caches.open(CATALOGUE_ASSETS).then((cache) => { 
+        return cache.match(evt.request); 
+      }); 
+    }) 
+  ); 
+}) 
