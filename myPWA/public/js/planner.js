e93bb0fb61ba92305e9e9659bd89b582b34b470c
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-02-26 19:27:27 +1100

    Display Task and Date Correctly

--- myPWA/public/js/planner.js
+++ myPWA/public/js/planner.js
@@ -1,46 +1,75 @@
 var taskArray = [];
-let result = "";
-var listTask = document.getElementById("content");
-let storedTask = localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
+var deadlineArray = [];
+let taskCard = "";
+//var listTask = document.getElementById("content");
+//let storedTask = localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
 
-window.onload = function () { 
+/* window.onload = function () {
+    console.log(storedTask);
     displayTask();
-}
+} */
+
 function addTask(){ //getting task from html 
-   var task = { 
-        newTask: document.getElementById("setTask").value,
-        deadline: document.getElementById("setDate").value,
-    };
-    document.getElementById("setTask").value = "";
-    document.getElementById("setDate").value = "";
-    saveTask(task);
-    window.location.reload(); //temporary fix
+      /* var task = { 
+            newTask: document.getElementById("setTask").value,
+            deadline: document.getElementById("setDate").value,
+        }; */
+        
+        var newTask = document.getElementById("setTask").value;
+        var deadline = document.getElementById("setDate").value;
+        taskArray.push(newTask);
+        deadlineArray.push(deadline);
+        console.log(taskArray);
+        console.log(deadline);
+
+        document.getElementById("setTask").value = "";
+        document.getElementById("setDate").value = "";
+        displayTask(); 
+
+       // location.reload(); //temporary fix 
 }
 
-function saveTask(task) { //saving task in local storage
+function displayTask() {
+    arrayIndex = 0;
+    htmlString = "";
+      while (arrayIndex < taskArray.length && deadlineArray.length) {
+        newItem = "<div "+ "class='task-card'>" +"<h2>" +"Task: " + taskArray[arrayIndex] + 
+        "</h2>"+ "<p "+ "class='deadline'>" + "Due Date:"+ deadlineArray[arrayIndex] + "</p>" + "<button " + "class= 'btn'" + "id='delete'"+ taskArray[arrayIndex] + "'onclick='removeListItem(" + arrayIndex + ")';>"+ 
+        "Delete Task" +"</button> " + "</div>";
+        htmlString = htmlString + newItem;
+        arrayIndex = arrayIndex + 1;
+        }
+        document.getElementById("content").innerHTML = htmlString;
+}
+
+
+
+
+/* function saveTask(task) { //saving task in local storage
    storedTask.push(task);
    localStorage.setItem('task', JSON.stringify(storedTask));
    console.log(storedTask);
+   //document.getElementById("list").innerHTML = "";
    displayTask();
 }
 
-function displayTask() { //displaying task in html 
-  // for (var i= 0; i < storedTask.length; i++) {
-        storedTask.forEach((tasks, index) => { 
-            result += ` 
-             <div id = ${index} class="task-card"> 
+function displayTask() { //displaying task in html
+       storedTask.forEach((tasks, index) => { //problem: recreates previous saved data
+            taskCard += `
+             <li id = ${index} class="task-card"> 
                   <h3 class="name">Task: ${tasks.newTask}</h3> 
                   <p class="deadline">Due Date: ${tasks.deadline}</p> 
-                  <button class="btn" id= "delete" onclick="removeTask(${index})"> Delete Task</button>
-              </div> 
+                  <button class="btn" id= "delete" onclick="removeTask(${index})">Delete Task</button>
+              </li> 
              `; 
           }); 
-  // }
-    document.getElementById("list").innerHTML = result;
+    document.getElementById("list").innerHTML = taskCard;
 }
 
 function removeTask(index) {
-    storedTask.splice(index); //why does this not work properly with , 1
-    localStorage.setItem('task', JSON.stringify(storedTask)); 
-    document.getElementById(index).remove();
-} 
+        storedTask.splice(this); //why does this not work properly with , 1
+        localStorage.setItem('task', JSON.stringify(storedTask)); 
+        document.getElementById(index).remove();
+        console.log(storedTask);
+       //location.reload(); //temporary fix
+}  */
