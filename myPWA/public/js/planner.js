f541106cdfd8995d12e20db1fb046f56c0cf3ca9
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-02-26 22:42:58 +1100

    Lots of testing
    - still figuring out to fix the issues where after you reload the task box content becomes undefined

--- myPWA/public/js/planner.js
+++ myPWA/public/js/planner.js
@@ -5,16 +5,12 @@ let storedTask = localStorage.getItem('task') ? JSON.parse(localStorage.getItem(
 
 window.onload = function () {
     displayTask();
-    localStorage.clear();
+   // localStorage.clear(); //to remove manually 
     console.log(storedTask);
 
 }
 
 function addTask(){ //getting task from html 
-      /* var task = { 
-            newTask: document.getElementById("setTask").value,
-            deadline: document.getElementById("setDate").value,
-        }; */
         var newTask = document.getElementById("setTask").value;
         var deadline = document.getElementById("setDate").value;
         taskArray.push(newTask);
@@ -25,24 +21,30 @@ function addTask(){ //getting task from html
         document.getElementById("setTask").value = "";
         document.getElementById("setDate").value = "";
         displayTask(); 
-
-       // location.reload(); //temporary fix 
 }
 
-function displayTask() {
+function displayTask() { //displaying task in html
     arrayIndex = 0;
     htmlString = "";
-      while (arrayIndex < taskArray.length && deadlineArray.length) {
-        newItem = "<div "+ "class='task-card'>" + "<h2>" +"Task: " + taskArray[arrayIndex] + 
-        "</h2>"+ "<p "+ "class='deadline'>" + "Due Date:"+ deadlineArray[arrayIndex] + "</p>" + "<button " + "class= 'btn'" + "id='delete'" + "onclick='removeTask(" + arrayIndex + ")';>"+
-        "Delete Task" +"</button> " + "</div>";
+   /* //let item = localStorage.getItem('task', JSON.stringify(storedTask));
+    for (var i=0; i <storedTask.length; i++) {
+        newItem = "<div "+ "class='task-card'>" + "<h2>" +"Task: " + storedTask.taskName + 
+        "</h2>"+ "<p "+ "class='deadline'>" + "Due Date:"+ storedTask.taskDate + "</p>" + 
+        "<button " + "class= 'btn'" + "id='delete'" + "onclick='removeTask(" + i + ")';>"+ "Delete Task" +"</button> " + "</div>";
+        htmlString = htmlString + newItem;
+    }; */
+    while (arrayIndex < storedTask.length) {
+        newItem = "<div "+ "class='task-card'>" + 
+        "<h2>" +"Task: " + taskArray[arrayIndex] + "</h2>"+
+         "<p "+ "class='deadline'>" + "Due Date:"+ deadlineArray[arrayIndex] + "</p>" + 
+         "<button " + "class= 'btn'" + "id='delete'" + "onclick='removeTask(" + arrayIndex + ")';>"+ "Delete Task" +"</button> " + "</div>";
         htmlString = htmlString + newItem;
         arrayIndex = arrayIndex + 1;
-        }
+        } 
         document.getElementById("content").innerHTML = htmlString;
 }
 
-function removeTask(i) {
+function removeTask(i) { //removing items form storage and display
     console.log("button works");
     taskArray.splice(i, 1);
     deadlineArray.splice(i, 1); 
@@ -64,6 +66,12 @@ function storingTask(newTask, deadline) {
     console.log(storedTask);
 }
 
+      /* var task = { 
+            newTask: document.getElementById("setTask").value,
+            deadline: document.getElementById("setDate").value,
+        }; */
+
+         // location.reload(); //temporary fix 
 
 /* function saveTask(task) { //saving task in local storage
    storedTask.push(task);
@@ -84,6 +92,17 @@ function displayTask() { //displaying task in html
              `; 
           }); 
     document.getElementById("list").innerHTML = taskCard;
+
+       /* var task = storedTask;
+    htmlString = "";
+    for (var i=0; i <task.length; i++) {
+        var data = JSON.parse(task[i]);
+        newItem = "<div "+ "class='task-card'>" + "<h2>" +"Task: " + data.taskName + 
+        "</h2>"+ "<p "+ "class='deadline'>" + "Due Date:"+ data.taskDate + "</p>" + "<button " + "class= 'btn'" + "id='delete'" + "onclick='removeTask("[i];")';>"+
+        "Delete Task" +"</button> " + "</div>";
+        htmlString = htmlString + newItem;
+    } 
+
 }
 
 function removeTask(index) {
